c82ffc107049d14c470a2f99b5464328368c47d2
CTL_YU <yuxingyu001@gmail.com>
2017-06-16 11:24:10 +0800

    第一次创建

---
+++ app/src/main/java/ctl/newcalendar/NewCalendar.java
@@ -0,0 +1,23 @@
+package ctl.newcalendar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by yu on 2017/6/15.
+ */
+
+public class NewCalendar extends LinearLayout{
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context,  AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context,  AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
