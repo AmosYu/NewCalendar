2927d86b1b93514f78463ae22abff522e2e330a6
CTL_YU <yuxingyu001@gmail.com>
2017-06-16 17:07:10 +0800

    增加自定义TextView

--- app/src/main/java/ctl/newcalendar/MainActivity.java
+++ app/src/main/java/ctl/newcalendar/MainActivity.java
@@ -2,12 +2,22 @@ package ctl.newcalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextViewBorder view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = (TextViewBorder) findViewById(R.id.state1);
+        view.setBorderColor(getResources().getColor(R.color.app_red_delete_color));
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.setBorderColor(getResources().getColor(R.color.buttong_check_color));
+            }
+        });
     }
 }
