b53384f8f3ea5cc0e250b6c12c6b69d5502f4a6e
yuxingyu <y_xingyu@126.com>
2017-10-26 14:24:10 +0800

    1

--- app/src/main/java/ctl/newcalendar/MainActivity.java
+++ app/src/main/java/ctl/newcalendar/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         String s3 = "123";
         String s4 = "123";
         int i = 3+4;
+        String s1 = "majiajue";
         view = (TextViewBorder) findViewById(R.id.state1);
         view = (TextViewBorder) findViewById(R.id.state1);
         view = (TextViewBorder) findViewById(R.id.state1);
